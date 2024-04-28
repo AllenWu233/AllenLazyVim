cae42dc89250c0a0541f95667dcf97fd3f0b1588
AllenWu233 <allenwu233@163.com>
2024-04-28 13:14:18 +0800

    Update plugins.lua

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -111,7 +111,7 @@ return {
   {
     "ahmedkhalf/project.nvim",
     opts = {
-      manual_mode = false,
+      manual_mode = false, -- Set false to auto cd worksapce folder
     },
   },
 
@@ -193,7 +193,7 @@ return {
           name = "Launch",
           type = "codelldb",
           request = "launch",
-          program = function() -- Ask the user what executable wants to debug
+          program = function()
             -- return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/bin/program", "file")
             return vim.fn.input(
               "Path to executable: ",
@@ -202,27 +202,6 @@ return {
             )
           end,
           cwd = "${workspaceFolder}",
-          -- stopOnEntry = false,
-          -- args = {},
-          -- initCommands = function() -- add rust types support (optional)
-          --   -- Find out where to look for the pretty printer Python module
-          --   local rustc_sysroot = vim.fn.trim(vim.fn.system("rustc --print sysroot"))
-          --
-          --   local script_import = 'command script import "' .. rustc_sysroot .. '/lib/rustlib/etc/lldb_lookup.py"'
-          --   local commands_file = rustc_sysroot .. "/lib/rustlib/etc/lldb_commands"
-          --
-          --   local commands = {}
-          --   local file = io.open(commands_file, "r")
-          --   if file then
-          --     for line in file:lines() do
-          --       table.insert(commands, line)
-          --     end
-          --     file:close()
-          --   end
-          --   table.insert(commands, 1, script_import)
-          --
-          --   return commands
-          -- end,
         },
       }
     end,
