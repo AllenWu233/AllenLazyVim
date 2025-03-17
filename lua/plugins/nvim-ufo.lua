d35e2e868f87c88a17fd5d75c678a7ea0876c635
AllenWu233 <allenwu233@outlook.com>
2025-03-17 13:00:08 +0800

    backup.sh 2025-03-17 13:00:08

--- lua/plugins/nvim-ufo.lua
+++ lua/plugins/nvim-ufo.lua
@@ -40,48 +40,50 @@
 --   },
 -- }
 
-return {
-  {
-    "kevinhwang91/nvim-ufo",
-    enabled = true,
-    dependencies = { "kevinhwang91/promise-async" },
-    event = "BufRead",
-    opts = {
-      close_fold_kinds_for_ft = {
-        default = { "imports" },
-      },
-      provider_selector = function(_, filetype)
-        return { "treesitter", "indent" }
-      end,
-    },
-    init = function()
-      vim.o.foldcolumn = "1"
-      vim.o.foldlevel = 99
-      vim.o.foldlevelstart = 99
-      vim.o.foldenable = true
-    end,
-    keys = {
-      {
-        "zR",
-        function()
-          require("ufo").openAllFolds()
-        end,
-      },
-      {
-        "zM",
-        function()
-          require("ufo").closeAllFolds()
-        end,
-      },
-      {
-        "K",
-        function()
-          local winid = require("ufo").peekFoldedLinesUnderCursor()
-          if not winid then
-            vim.lsp.buf.hover()
-          end
-        end,
-      },
-    },
-  },
-}
+-- return {
+--   {
+--     "kevinhwang91/nvim-ufo",
+--     enabled = true,
+--     dependencies = { "kevinhwang91/promise-async" },
+--     event = "BufRead",
+--     opts = {
+--       close_fold_kinds_for_ft = {
+--         default = { "imports" },
+--       },
+--       provider_selector = function(_, filetype)
+--         return { "treesitter", "indent" }
+--       end,
+--     },
+--     init = function()
+--       vim.o.foldcolumn = "1"
+--       vim.o.foldlevel = 99
+--       vim.o.foldlevelstart = 99
+--       vim.o.foldenable = true
+--     end,
+--     keys = {
+--       {
+--         "zR",
+--         function()
+--           require("ufo").openAllFolds()
+--         end,
+--       },
+--       {
+--         "zM",
+--         function()
+--           require("ufo").closeAllFolds()
+--         end,
+--       },
+--       {
+--         "K",
+--         function()
+--           local winid = require("ufo").peekFoldedLinesUnderCursor()
+--           if not winid then
+--             vim.lsp.buf.hover()
+--           end
+--         end,
+--       },
+--     },
+--   },
+-- }
+
+return {}
