2437e7bb1e7ca9996bf8b6ff41be4578c8ea7beb
AllenWu233 <allenwu233@outlook.com>
2025-03-17 17:00:08 +0800

    backup.sh 2025-03-17 17:00:08

--- lua/plugins/nvim-ufo.lua
+++ lua/plugins/nvim-ufo.lua
@@ -1,59 +1,10 @@
+return {}
 -- return {
 --   {
 --     "kevinhwang91/nvim-ufo",
+--     enabled = false,
 --     dependencies = { "kevinhwang91/promise-async" },
 --     event = "BufRead",
---     keys = {
---       {
---         "zR",
---         function()
---           require("ufo").openAllFolds()
---         end,
---       },
---       {
---         "zM",
---         function()
---           require("ufo").closeAllFolds()
---         end,
---       },
---       {
---         "K",
---         function()
---           local winid = require("ufo").peekFoldedLinesUnderCursor()
---           if not winid then
---             vim.lsp.buf.hover()
---           end
---         end,
---       },
---     },
---     config = function()
---       vim.o.foldcolumn = "1"
---       vim.o.foldlevel = 99
---       vim.o.foldlevelstart = 99
---       vim.o.foldenable = true
---       require("ufo").setup({
---         close_fold_kinds_for_ft = {
---           default = { "imports" },
---         },
---       })
---     end,
---   },
--- }
-
--- return {
---   {
---     "kevinhwang91/nvim-ufo",
---     enabled = true,
---     dependencies = { "kevinhwang91/promise-async" },
---     event = "BufRead",
---     opts = {
---       close_fold_kinds_for_ft = {
---         default = { "imports" },
---       },
---       provider_selector = function(_, filetype)
---         return { "treesitter", "indent" }
---       end,
---     },
 --     init = function()
 --       vim.o.foldcolumn = "1"
 --       vim.o.foldlevel = 99
@@ -84,6 +35,12 @@
 --       },
 --     },
 --   },
+--   opts = {
+--     close_fold_kinds_for_ft = {
+--       default = { "imports" },
+--     },
+--     provider_selector = function(_, filetype)
+--       return { "treesitter", "indent" }
+--     end,
+--   },
 -- }
-
-return {}
